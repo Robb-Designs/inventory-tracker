ff068a938cf34f705795a3b88e529b3fe6dfc6a4
Robb-Designs <rtlee@aggies.ncat.edu>
2026-04-03 21:27:53 -0400

    made main logic for inventory tracker, created instances with imported class and subclasses

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import PhysicalProduct from "./models/PhysicalProduct.js";
+import DigitalProduct from "./models/DigitalProduct.js";
+import Product from "./models/Product.js";
+
+
+//instances
+const dellLaptop = new PhysicalProduct("SKU1103", "Dell G15", 599, 1000);
+const eBook = new DigitalProduct("SKU7995", "Kindl Shrek III", 5, 36)
+//setting variable using polymorphism for looping
+const inventory: Product[] = [dellLaptop, eBook];
+
+//functions---------------------------------
+function looper(arr: Product[]) {
+    for (let i = 0; i < arr.length; i++)
+        console.log(arr[i].displayDetails());
+        console.log("Final Price:", arr[i].getPriceWithTax());
+       
+}
+
+looper(inventory);
