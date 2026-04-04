50857cdbcf9e4e557e838109599695444851d51c
Robb-Designs <rtlee@aggies.ncat.edu>
2026-04-03 20:34:32 -0400

    refactoredd the logic for physical product sub class

---
+++ src/models/PhysicalProduct.ts
@@ -0,0 +1,24 @@
+// importing product class from Product.ts
+import Product from "./Product.ts";
+
+export class PhysicalProduct extends Product {
+    protected weight: number;
+
+    constructor(sku: string, name: string, price: number, weight: number) {
+        super(sku, name, price);
+        this.weight = weight;
+    }
+
+    protected getTaxRate(): number {
+        return 0.10;
+    }
+
+    // getter to return formatted weight
+    get formattedWeight(): string {
+        return `${this.weight} kg`;
+    }
+
+
+}
+
+//console.log(PhysicalProduct())
